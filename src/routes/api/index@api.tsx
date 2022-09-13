4fbdcb0cff6252cbd959599b83e21bd75aa6e3b7
Steve Randy Tantra <525332+steverandy@users.noreply.github.com>
2022-09-13 11:07:06 +0800

    update src/routes/api/index@api.tsx

--- src/routes/api/index@api.tsx
+++ src/routes/api/index@api.tsx
@@ -1,11 +1,18 @@
-import { component$, useClientEffect$, useStore } from '@builder.io/qwik';
+import {
+  component$,
+  Resource,
+  useClientEffect$,
+  useStore,
+} from "@builder.io/qwik";
+import { useEndpoint } from "@builder.io/qwik-city";
 
 /**
  * Uses named layout `foo`
  */
 
 export default component$(() => {
-  const store = useStore({ timestamp: '' });
+  const store = useStore({ timestamp: "" });
+  const resource = useEndpoint();
 
   useClientEffect$(async () => {
     const url = `/api/builder.io/oss.json`;
@@ -28,7 +35,40 @@ export default component$(() => {
         </li>
       </ul>
 
-      <p>Timestamp: {store.timestamp}</p>
+      <Resource
+        value={resource}
+        onResolved={(data: any) => {
+          console.log("data", data);
+          return (
+            <div>
+              <p>Timestamp: {store.timestamp}</p>
+              {data.map((item: any) => item)}
+            </div>
+          );
+        }}
+      />
+
+      <div>
+        <button
+          class="bg-gray-200 p-2 rounded-md"
+          onClick$={() => {
+            fetch("/api", {
+              method: "POST",
+              headers: { accept: "application/json" },
+            });
+          }}
+        >
+          Test POST request
+        </button>
+      </div>
     </div>
   );
 });
+
+export let onGet = async () => {
+  return [1, 2, 3];
+};
+
+export let onPost = async () => {
+  return {};
+};
